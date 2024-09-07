a61b5c58491da132973f634a3da437a0e71ec00d
Flavio Nascimento Nunes <flavionascimentonunes2023@gmail.com>
2024-09-07 19:39:03 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,34 @@
+let dados =[
+    {
+        titulo : "Android studio",
+        descricao : "O Android Studio é a ferramenta oficial da Google para desenvolvimento de aplicativos para o sistema operacional Android. Ele oferece um ambiente de desenvolvimento integrado (IDE) completo, com diversas funcionalidades que facilitam a criação de apps de alta qualidade.",
+        link : "https://developer.android.com/studio?gad_source=1&gclid=CjwKCAjwreW2BhBhEiwAavLwfO4sBNuWF4trdFmkF7UlGR9Bj-Gl3gVndvIF5x4fvhZJAz5KaHh_nhoCd5sQAvD_BwE&gclsrc=aw.ds&hl=pt-br",
+        tags : "Desenvolvimento de aplicativos android emulator memoria ram desenvolver aplicativo android desenvolver aplicativo"
+    },
+    {
+        titulo : "Visual studio",
+        descricao : "Visual Studio Code: Um Editor de Código Versátil e Personalizável.O Visual Studio Code (comumente chamado de VS Code) é um editor de código fonte extremamente popular, desenvolvido pela Microsoft.",
+        link : "https://code.visualstudio.com/download",
+        tags : "aplicativo de programação criar site java javascript html desenvolver site "
+    },
+    {
+        titulo : "Cisco Packet tracer",
+        descricao : "Cisco Packet Tracer: Seu Laboratório de Redes Virtual.O Cisco Packet Tracer é uma ferramenta de simulação de redes de computadores amplamente utilizada no ensino e aprendizado de redes de computadores, especialmente em cursos de Cisco.",
+        link : "https://www.packettracernetwork.com/download/download-packet-tracer.html",
+        tags : "Redes de computadores simulador "
+    },
+    {
+        titulo: "Maquina Virtual Vmware",
+        descricao : "Uma máquina virtual VMware é como um computador dentro de um computador. Ela permite que você execute diversos sistemas operacionais diferentes (como Windows, Linux, etc.) em um único computador físico.",
+        link : "https://www.vmware.com/products/desktop-hypervisor/workstation-and-fusion",
+        tags : "um computador dentro de outro Linux "
+    },
+    {
+        titulo: "My SQL",
+        descricao : "O MySQL é um banco de dados relacional popular e de código aberto, utilizado para armazenar e organizar informações de forma estruturada. Ele é como um arquivo super organizado onde você pode guardar dados de um site, aplicativo ou qualquer outro sistema que precise armazenar informações.",
+        link: "https://dev.mysql.com/downloads/installer/",
+        tags : "banco de dados sql tabelas criar tabela inserir dados"
+    },
+];
+
+ 
